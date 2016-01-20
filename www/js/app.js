e7bd4c5592289e3e9ef11c9d5f49950a949efaca
Matias <matiastucci@gmail.com>
2016-01-20 02:47:02 +0100

    remove unnecessary comment

--- www/js/app.js
+++ www/js/app.js
@@ -18,10 +18,6 @@ angular.module('adra', ['ionic', 'adra.controllers', 'adra.services'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
   .state('tab', {
